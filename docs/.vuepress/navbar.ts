65c1c923f1e37dd917baf2daca207205cbd54df8
2290774104 <2290774104@qq.com>
2024-10-11 10:06:05 +0800

    docs: 增加使用指南文档

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,5 +1,8 @@
 import type { NavItem } from 'vuepress/config';
 
-const navbar: NavItem[] = [];
+const navbar: NavItem[] = [
+  { text: '主页', link: '/' },
+  { text: '指南', link: '/pages/guide/installation' },
+];
 
 export default navbar;
--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -1,5 +1,18 @@
 import { SidebarConfig4Multiple } from 'vuepress/config';
 
-const sidebar: SidebarConfig4Multiple = {};
+const sidebar: SidebarConfig4Multiple = {
+  '/pages/guide/': [
+    {
+      title: '安装',
+      collapsable: false,
+      path: '/pages/guide/installation',
+    },
+    {
+      title: '快速开始',
+      collapsable: false,
+      path: '/pages/guide/quickstart',
+    },
+  ],
+};
 
 export default sidebar;
