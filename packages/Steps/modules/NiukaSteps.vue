a67d3acc41a19923708103a6627341e2b0f12345
2290774104 <2290774104@qq.com>
2024-10-14 10:12:43 +0800

    refactor: 增加代码注释

--- packages/Steps/modules/NiukaSteps.vue
+++ packages/Steps/modules/NiukaSteps.vue
@@ -6,10 +6,13 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 import NiukaStepsItem from '../components/NiukaStepsItem.vue';
 @Component({ name: 'NiukaSteps', components: { NiukaStepsItem } })
 export default class NiukaSteps extends Vue {
+  // 当前激活步骤
   @Prop({ type: Number, required: true }) readonly active!: number;
 
+  // 步骤配置
   @Prop({ type: Array, required: true }) readonly steps!: IStep[];
 
+  // 每个步骤直接的间距
   @Prop({ type: Number, default: 154 }) readonly space!: number;
 
   private defaultIcon = ['filter', 'edit', 'setting'];
