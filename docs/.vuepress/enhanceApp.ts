c200dd7089d9b5e41def2adcb254b6fd1dd97993
2290774104 <2290774104@qq.com>
2024-10-17 09:46:35 +0800

    docs: 调整文档组件加载策略

--- docs/.vuepress/enhanceApp.ts
+++ docs/.vuepress/enhanceApp.ts
@@ -1,7 +1,6 @@
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-import NiukaComponents from '../../packages';
 
 import OverviewList from './components/common/OverviewList.vue';
 import PreviewComponents from './components/common/PreviewComponents.vue';
@@ -13,14 +12,19 @@ import http from '../../http'
 
 export default ({ Vue }) => {
   Vue.use(ElementUI);
-  Vue.use(NiukaComponents);
-  // Vue.mixin({
-  //   mounted() {
-  //     import('../../packages').then((m) => {
-  //       Vue.use(m.default)
-  //     })
-  //   }
-  // })
+  Vue.mixin({
+    mounted() {
+      // 动态引入组件
+      // 文档使用node打包，无法识别 window document 等浏览器对象
+      // 参考官方给出的方法，使用 vue 动态组件注册
+      import('../../packages').then((m) => {
+        Vue.use(m.default)
+        // 注册完成后重新渲染，但组件是在页面加载完成后注册
+        // 首次加载页面时组件会无法渲染，需要重新加载
+        this.$forceUpdate()
+      })
+    }
+  })
   Vue.component('OverviewList', OverviewList);
   Vue.component('preview-components', PreviewComponents);
   Vue.component('attribute-table', AttributeTable);
