23fe15b54cf19beac3e9b990d36ae865511cf24d
2290774104 <2290774104@qq.com>
2025-02-11 09:45:31 +0800

    docs: 组件预览部分图标添加

--- docs/.vuepress/components/common/SvgIcon/index.vue
+++ docs/.vuepress/components/common/SvgIcon/index.vue
@@ -7,10 +7,25 @@
 <script lang="ts">
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
-import IconSwitch from './switch.vue';
+import IconSwitch from './modules/Switch.vue';
+import IconSelect from './modules/Select.vue';
+import IconTagsInput from './modules/TagsInput.vue';
+import IconSelectTree from './modules/SelectTree.vue';
+import IconSelectColumn from './modules/SelectColumn.vue';
+import IconTable from './modules/Table.vue'
 
 // @ts-ignore
-@Component({ name: 'SvgIcon', components: { IconSwitch } })
+@Component({
+  name: 'SvgIcon',
+  components: {
+    IconSwitch,
+    IconSelect,
+    IconTagsInput,
+    IconSelectTree,
+    IconSelectColumn,
+    IconTable,
+  },
+})
 export default class SvgIcon extends Vue {
   // @ts-ignore
   @Prop({ type: String, required: true }) private readonly icon!: string;
---
+++ docs/.vuepress/components/common/SvgIcon/modules/Table.vue
@@ -0,0 +1,94 @@
+<template>
+  <svg
+    width="150"
+    height="75"
+    viewBox="0 0 150 75"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="150" height="75" fill="#F5F5F5" />
+    <path
+      d="M0 3C0 1.34315 1.34315 0 3 0H147C148.657 0 150 1.34315 150 3V72C150 73.6569 148.657 75 147 75H3C1.34314 75 0 73.6569 0 72V3Z"
+      fill="white"
+    />
+    <path
+      d="M0 3C0 1.34315 1.34315 0 3 0H147C148.657 0 150 1.34315 150 3V12H0L0 3Z"
+      fill="#E4E7ED"
+    />
+    <rect x="6.3" y="5.3" width="10" height="2" rx="1" fill="#D4D7DE" />
+    <rect x="42.3" y="5.3" width="10" height="2" rx="1" fill="#D4D7DE" />
+    <rect x="78.3" y="5.3" width="10" height="2" rx="1" fill="#D4D7DE" />
+    <rect x="134.3" y="5.3" width="10" height="2" rx="1" fill="#D4D7DE" />
+    <mask
+      id="path-6-outside-1_10_2"
+      maskUnits="userSpaceOnUse"
+      x="0"
+      y="27.05"
+      width="150"
+      height="2"
+      fill="black"
+    >
+      <rect fill="white" y="27.05" width="150" height="2" />
+      <path d="M0 28.05H150V28.0501H0V28.05Z" />
+    </mask>
+    <path
+      d="M150 27.5501H0V28.5501H150V27.5501Z"
+      fill="#F2F4F6"
+      mask="url(#path-6-outside-1_10_2)"
+    />
+    <mask
+      id="path-8-outside-2_10_2"
+      maskUnits="userSpaceOnUse"
+      x="0"
+      y="43.05"
+      width="150"
+      height="2"
+      fill="black"
+    >
+      <rect fill="white" y="43.05" width="150" height="2" />
+      <path d="M0 44.05H150V44.0501H0V44.05Z" />
+    </mask>
+    <path
+      d="M150 43.5501H0V44.5501H150V43.5501Z"
+      fill="#F2F4F6"
+      mask="url(#path-8-outside-2_10_2)"
+    />
+    <mask
+      id="path-10-outside-3_10_2"
+      maskUnits="userSpaceOnUse"
+      x="0"
+      y="59.05"
+      width="150"
+      height="2"
+      fill="black"
+    >
+      <rect fill="white" y="59.05" width="150" height="2" />
+      <path d="M0 60.05H150V60.0501H0V60.05Z" />
+    </mask>
+    <path
+      d="M150 59.5501H0V60.5501H150V59.5501Z"
+      fill="#F2F4F6"
+      mask="url(#path-10-outside-3_10_2)"
+    />
+    <rect x="6.3" y="18.3" width="16" height="4" rx="2" fill="#409EFF" />
+    <rect x="6.3" y="34.3" width="16" height="4" rx="2" fill="#409EFF" />
+    <rect x="6.3" y="50.3" width="16" height="4" rx="2" fill="#409EFF" />
+    <rect x="6.3" y="66.3" width="16" height="4" rx="2" fill="#409EFF" />
+    <rect x="42.3" y="18.3" width="20" height="4" rx="2" fill="#D4D7DE" />
+    <rect x="42.3" y="34.3" width="20" height="4" rx="2" fill="#D4D7DE" />
+    <rect x="42.3" y="50.3" width="20" height="4" rx="2" fill="#D4D7DE" />
+    <rect x="42.3" y="66.3" width="20" height="4" rx="2" fill="#D4D7DE" />
+    <rect x="78.3" y="18.3" width="20" height="4" rx="2" fill="#D4D7DE" />
+    <rect x="78.3" y="34.3" width="20" height="4" rx="2" fill="#D4D7DE" />
+    <rect x="78.3" y="50.3" width="20" height="4" rx="2" fill="#D4D7DE" />
+    <rect x="78.3" y="66.3" width="20" height="4" rx="2" fill="#D4D7DE" />
+    <rect x="132.3" y="18.3" width="12" height="4" rx="2" fill="#D4D7DE" />
+    <rect x="132.3" y="34.3" width="12" height="4" rx="2" fill="#D4D7DE" />
+    <rect x="132.3" y="50.3" width="12" height="4" rx="2" fill="#D4D7DE" />
+    <rect x="132.3" y="66.3" width="12" height="4" rx="2" fill="#D4D7DE" />
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
