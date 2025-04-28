1a63e2b89c6459b50f8cb58ba52ea40c2521d6ec
2290774104 <2290774104@qq.com>
2025-04-28 09:36:28 +0800

    fix: 修复SelectColumn空数据处理错误

--- packages/SelectColumn/modules/NiukaSelectColumn.vue
+++ packages/SelectColumn/modules/NiukaSelectColumn.vue
@@ -77,7 +77,7 @@ export default class NiukaSelectColumn extends Vue {
   }
 
   private initData(data: IData[]): TreeData[] {
-    return data.filter(i => i.catid).map((i) => {
+    return data.map((i) => {
       return {
         id: i.catid,
         label: i.name,
